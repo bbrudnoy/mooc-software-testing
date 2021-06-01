34fe8d6b37de7e2540164925bfeadccbaf911008
Boris Brudnoy <boris.brudnoy@healthometry.com>
2021-05-31 21:51:03 -0400

    Mirror and GHappy exercise tests
    Mirror and GHappy exercise tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,16 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GHappyTest {
+
+  @ParameterizedTest
+  @CsvSource({"gg, true"})
+  public void shortHappy(String string, boolean expected) {
+    final GHappy gHappy = new GHappy();
+    assertEquals(expected, gHappy.gHappy(string));
+  }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,31 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+
+  @ParameterizedTest
+  @CsvSource({ "aba, aba", "ab, abXYZba", "abcba, abcba", "a, a" })
+  public void oddNumOfCharacters(String expectedResult, String string) {
+    final Mirror mirror = new Mirror();
+    Assertions.assertEquals(expectedResult, mirror.mirrorEnds(string));
+  }
+
+
+  @ParameterizedTest
+  @CsvSource({ "a, abca", "ab, abSTba" })
+  public void evenNumOfCharacters(String expectedResult, String string) {
+    final Mirror mirror = new Mirror();
+    Assertions.assertEquals(expectedResult, mirror.mirrorEnds(string));
+  }
+
+
+  @ParameterizedTest
+  @CsvSource({ "aa, aa", "abba, abba" })
+  public void evenPalindromes(String expectedResult, String string) {
+    final Mirror mirror = new Mirror();
+    Assertions.assertEquals(expectedResult, mirror.mirrorEnds(string));
+  }
 }
