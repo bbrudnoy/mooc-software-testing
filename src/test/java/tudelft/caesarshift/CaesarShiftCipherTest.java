116d35a34c73e519422bc9cd2c4ce51d752704bc
Boris Brudnoy <boris.brudnoy@healthometry.com>
2021-06-01 22:05:30 -0400

    Fixed CaesarShiftCipher via Equivalence Partitioning
    Fixed CaesarShiftCipher via Equivalence Partitioning

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,53 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests {@link CaesarShiftCipherTest}.
+ * <p>
+ * Used <a href="https://www.dcode.fr/caesar-cipher">https://www.dcode.fr/caesar-cipher</a> as reference.
+ * </p>
+ */
 public class CaesarShiftCipherTest {
 
+  @Disabled
+  @ParameterizedTest
+  @ValueSource(ints = { -26, 0, 26 })
+  public void noShift(int neutralShift) {
+    final CaesarShiftCipher cipher = new CaesarShiftCipher();
+    assertEquals("good day", cipher.CaesarShiftCipher("good day", neutralShift));
+  }
+
+
+  @Test
+  public void positiveShiftWithoutSpaces() {
+    final CaesarShiftCipher cipher = new CaesarShiftCipher();
+    assertEquals("khoor", cipher.CaesarShiftCipher("hello", 3));
+  }
+
+
+  @Test
+  public void positiveShiftWithSpaces() {
+    final CaesarShiftCipher cipher = new CaesarShiftCipher();
+    assertEquals("jrrg gdb", cipher.CaesarShiftCipher("good day", 3));
+  }
+
+
+  @Test
+  public void negativeShiftWithoutSpaces() {
+    final CaesarShiftCipher cipher = new CaesarShiftCipher();
+    assertEquals("ebiil", cipher.CaesarShiftCipher("hello", -3));
+  }
+
+
+  @Test
+  public void negativeShiftWithSpaces() {
+    final CaesarShiftCipher cipher = new CaesarShiftCipher();
+    assertEquals("dlla axv", cipher.CaesarShiftCipher("good day", -3));
+  }
 }
